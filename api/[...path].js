b50b614aa848e7906c9c3a06f245ef7ddf84c626
fariha-shahbaz-498 <farihashahbaz498@gmail.com>
2026-08-30 13:09:24 -0700

    Fix Vercel serverless API function

--- api/[...path].js
+++ api/[...path].js
@@ -1,6 +1,23 @@
 import app from "../server/src/app.js";
 import connectDB from "../server/src/config/db.js";
 
-await connectDB();
+let dbConnected = false;
 
-export default app;
+export default async function handler(req, res) {
+  try {
+    if (!dbConnected) {
+      await connectDB();
+      dbConnected = true;
+    }
+
+    return app(req, res);
+  } catch (error) {
+    console.error("API ERROR:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Server error",
+      error: error.message,
+    });
+  }
+}
